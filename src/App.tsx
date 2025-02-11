c93dec764ec4789f5a20b533b191282a4bb9d461
Joordih <harpymclol@gmail.com>
2025-02-11 15:53:43 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { Route, Routes } from "react-router-dom";
+
+import IndexPage from "@/pages/index";
+import DocsPage from "@/pages/docs";
+import PricingPage from "@/pages/pricing";
+import BlogPage from "@/pages/blog";
+import AboutPage from "@/pages/about";
+import SP500Page from "@/pages/sp500";
+
+function App() {
+  return (
+    <Routes>
+      <Route element={<IndexPage />} path="/" />
+      <Route element={<SP500Page />} path="/sp500" />
+      <Route element={<DocsPage />} path="/docs" />
+      <Route element={<PricingPage />} path="/pricing" />
+      <Route element={<BlogPage />} path="/blog" />
+      <Route element={<AboutPage />} path="/about" />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ src/config/site.ts
@@ -0,0 +1,65 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Innvest",
+  description: "The best check stock apps.",
+  navItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "S&P 500",
+      href: "/sp500",
+    },
+    // {
+    //   label: "Pricing",
+    //   href: "/pricing",
+    // },
+    // {
+    //   label: "Blog",
+    //   href: "/blog",
+    // },
+    {
+      label: "About",
+      href: "/about",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "Profile",
+      href: "/profile",
+    },
+    {
+      label: "Dashboard",
+      href: "/dashboard",
+    },
+    {
+      label: "Projects",
+      href: "/projects",
+    },
+    {
+      label: "Team",
+      href: "/team",
+    },
+    {
+      label: "Calendar",
+      href: "/calendar",
+    },
+    {
+      label: "Settings",
+      href: "/settings",
+    },
+    {
+      label: "Help & Feedback",
+      href: "/help-feedback",
+    },
+    {
+      label: "Logout",
+      href: "/logout",
+    },
+  ],
+  links: {
+    github: "https://github.com/joordih/innvest",
+  },
+};
